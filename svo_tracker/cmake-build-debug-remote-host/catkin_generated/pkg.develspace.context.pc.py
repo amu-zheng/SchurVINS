e21dc5f93a727affe4dd9b11e75957a220c00e8b
root <zhengamu@embodiedai.ai>
2024-07-29 15:45:07 +0800

    Refresh cach and rebuild index

--- svo_tracker/cmake-build-debug-remote-host/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/tmp/tmp.EjzChWJKnL/svo_tracker/include".split(';') if "/tmp/tmp.EjzChWJKnL/svo_tracker/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "roslib;glog_catkin;cv_bridge;eigen_catkin;svo_common;svo_direct;svo_test_utils".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lsvo_tracker".split(';') if "-lsvo_tracker" != "" else []
-PROJECT_NAME = "svo_tracker"
-PROJECT_SPACE_DIR = "/tmp/tmp.EjzChWJKnL/svo_tracker/cmake-build-debug-remote-host/devel"
-PROJECT_VERSION = "0.0.1"
--- svo_tracker/cmake-build-debug-remote-host/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /tmp/tmp.EjzChWJKnL/svo_tracker/cmake-build-debug-remote-host/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
